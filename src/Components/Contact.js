1e85093c23ba755a3cd40ddbc9c56640dfe73393
Aryan Chourey <aryanchourey4@gmail.com>
2022-09-07 17:07:51 +0530

    heroku link

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -33,7 +33,7 @@ export default function Contact() {
       icon: false,
       duration: 20000
     });
-    fetch("http://localhost:8080/submit-query", {
+    fetch("https://aryanchoureybackend.herokuapp.com/submit-query", {
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
